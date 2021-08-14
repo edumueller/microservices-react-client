eaf95a28fbc3bef52ea0f2ce827c7c593a0986a3
edumueller <eduardo.nicastro@icloud.com>
2021-08-14 17:17:58 -0300

    feat(post-list): add posts fetch request on render

--- src/PostList.js
+++ src/PostList.js
@@ -1,6 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const PostList = () => {
+  const [posts, setPosts] = useState({});
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
   return <div />;
 };
 
